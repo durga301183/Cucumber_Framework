64331d0166bcdd6a0fcd4b17f0135e168ee333b1
G S Sastry <sh.makkapati@gmail.com>
2020-06-28 15:26:14 +0100

    Second commit with changes

--- src/test/java/TestRunner/Hooks.java
+++ src/test/java/TestRunner/Hooks.java
@@ -9,12 +9,17 @@ public class Hooks extends BaseTest  {
 
     @Before ("@Test")
     public void openBrowser(){
+
+        System.out.println("This is opening browser");
+
         BaseTest.openDriver();
     }
 
 
     @After("@Test")
     public void closeBrowser(){
+
+        System.out.println("This is closing browser");
         BaseTest.closeDriver();
    }
 
